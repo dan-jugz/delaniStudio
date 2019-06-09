482e808acfbc8e775b5d608fa00a22571251847d
daniel njuguna <njugunadaniel364@gmail.com>
2019-06-09 09:24:54 +0300

    add email response functionality

--- js/scripts.js
+++ js/scripts.js
@@ -24,5 +24,16 @@ $(document).ready(function () {
   }, function () {
     $(this).animate({ opacity: '0' });
   })
+  $('#submission').submit(function (event) {
+    var display1 = $('input:first').val();
+    var display2 = $('input#mail').val();
+    if (display1 && display2 != '') {
+      alert('Thank you ' + display1 + ' your subscription is well received');
+    } else {
+      alert('Invalid input');
+    }
+    event.preventDefault();
+  })
   
+  });
 });
